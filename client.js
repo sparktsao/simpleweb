06a55d2629f5d66c4377175f98234291eb6fb9d9
EC2 Default User <ec2-user@ip-172-31-36-200.us-west-2.compute.internal>
2017-07-23 10:45:58 +0000

    refine ur

--- client.js
+++ client.js
@@ -8,8 +8,28 @@ $("#data").change(function(){
 	  cache: false,
 	  success: function(data){
 	     $("#resultarea").text(data);
+             if(data.split("\n").slice(-1)>0.5)
+             {
+                document.getElementById("score").style.background='red';
+                document.getElementById("score").value = data.split("\n").slice(-1);
+                document.getElementById("score").innerHTML = "Dangerous, score:"+data.split("\n").slice(-1);
+                console.log(data.split("\n").slice(-1));
+             }
+             else
+             {
+                document.getElementById("score").style.background='green';
+                document.getElementById("score").value = data.split("\n").slice(-1);
+                document.getElementById("score").innerHTML = data.split("\n").slice(-1);
+                console.log(data.split("\n").slice(-1));
+             }
 	  }
 	});
 
   });
 
+$("#data").mouseenter(function(){
+    $("#data").css("background-color", "yellow");
+});
+$("#data").mouseleave(function(){
+    $("#data").css("background-color", "white");
+});
